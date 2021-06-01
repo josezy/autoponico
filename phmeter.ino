a4c459848b813f57eb12e61be36e8792dbb22e88
Atlejo <alejandro.naranjo@ecosystems.solutions>
2021-06-01 16:26:37 -0500

    water drops functions implemented to future ph control

--- phmeter.ino
+++ phmeter.ino
@@ -1,36 +1,42 @@
-#include "TM1637.h"
+//#include "TM1637.h"
 
-#define M1 5
-#define M2 6
-#define M_SPEED 200
+#define M1 9
+#define M2 10
+#define M1_SPEED 130
+#define M2_SPEED 170
+#define DEFAULT_SPEED 0
 
 #define PH_PIN A7
 
 #define SEG7_CLK 6
 #define SEG7_DIO 7
 
-TM1637 tm1637(SEG7_CLK, SEG7_DIO);
+#define WATER_DROP_TIME 100
+
+//TM1637 tm1637(SEG7_CLK, SEG7_DIO);
 
 void setup() {
-    pinMode(13, OUTPUT);
     pinMode(M1, OUTPUT);
     pinMode(M2, OUTPUT);
-    analogWrite(M1, M_SPEED);
-    analogWrite(M2, M_SPEED);
+    
     Serial.begin(115200);
 
-    tm1637.init();
-    tm1637.set(BRIGHT_TYPICAL);
-    float num = 12.8;
-    Serial.println(num);
-    tm1637.displayNum(num, 2, false);
+    // tm1637.init();
+    // tm1637.set(BRIGHT_TYPICAL);
+    // float num = 12.8;
+    // Serial.println(num);
+    // tm1637.displayNum(num, 2, false);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(500);
-    digitalWrite(13, LOW);
-    delay(500);
+    pH_up();
+    calm_down();
+    delay(5000);
+    
+    pH_down();
+    calm_down();
+    delay(5000);
+    
     Serial.print("Pot: ");
     Serial.print(analogRead(A0));
     Serial.print("\tpH: ");
@@ -47,4 +53,21 @@ float get_pH() {
     }
     float voltage = 5 / 1024.0 * measurings / samples;
     return 7 + ((2.5 - voltage) / 0.18);
+}
+
+void pH_up() {
+    analogWrite(M2, DEFAULT_SPEED);
+    analogWrite(M1, M1_SPEED);
+    delay(WATER_DROP_TIME);
+}
+
+void pH_down() {
+    analogWrite(M1, DEFAULT_SPEED);
+    analogWrite(M2, M2_SPEED);
+    delay(WATER_DROP_TIME);
+}
+
+void calm_down() {
+    analogWrite(M1, DEFAULT_SPEED);
+    analogWrite(M2, DEFAULT_SPEED);
 }
