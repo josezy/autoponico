cb0d14d25ed27e773d9d15d71b81266ab1b88902
abcei2 <santiago.salgado.duque@gmail.com>
2022-04-07 13:31:58 -0500

    desired ph setting by serial and save on eeprom

--- autoponico.ino
+++ autoponico.ino
@@ -1,12 +1,18 @@
 #include <Arduino_JSON.h>
 #include <ShiftRegister74HC595.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 #define M_PH_UP 8
 #define M_PH_DN 9
 #define M_PH_UP_SPEED 130
 #define M_PH_DN_SPEED 170
 #define ZERO_SPEED 0
+//pH 5.0 => 9.0
+//pHeeprom  0 => 255
+#define MAX_DESIRED_PH 9.0
+#define MIN_DESIRED_PH 5.0
+#define DESIRED_PH_ADDRESS 0
 
 #define POT_PIN A0
 
@@ -63,10 +69,12 @@ uint8_t secondDisplay[] = {
 };
 
 boolean manualMode = true;
-JSONVar Data;
 
 float pH;
 float desired_pH;
+float desired_pH_cmd  ;
+bool desired_pH_from_cmd = false;
+
 float error;
 
 String sensorstring = "";
@@ -104,7 +112,18 @@ void test_system() {
         delay(50);
     }
 }
+//----------------------//
+//---- EEPROM  ---------//
+void desired_pH_to_eeprom(float cmd_pH) {
+    EEPROM.write(DESIRED_PH_ADDRESS, map((int)(cmd_pH*10), MIN_DESIRED_PH*10, MAX_DESIRED_PH*10, 0, 255));
+}
 
+float desired_pH_from_eeprom(){
+    return map(EEPROM.read(DESIRED_PH_ADDRESS), 0, 255, MIN_DESIRED_PH*10, MAX_DESIRED_PH*10)/10.0
+}
+//----------------------//
+
+//---- MAIN  ---------//
 void setup() {
     pinMode(M_PH_UP, OUTPUT);
     pinMode(M_PH_DN, OUTPUT);
@@ -112,19 +131,17 @@ void setup() {
 
     Serial.begin(9600);
     pH_Serial.begin(9600);
-
+    desired_pH_cmd = desired_pH_from_eeprom();
     // test_system();
 }
 
 void loop() {
-    pH = get_pH();
-    desired_pH = get_desired_pH();
-    show_in_current_display(pH);
-    show_in_desired_display(desired_pH);
-    check_for_command();
+   
+    get_measure_error_and_show();
+    print_measure_and_setpoint();
+    check_for_command();               
 
     if (!manualMode) {
-        error = pH - desired_pH;
 
         if (abs(error) >= ERR_MARGIN) {
             do {
@@ -137,20 +154,13 @@ void loop() {
                 }
 
                 long start = millis();
-                while (millis() - start < STABILIZATION_TIME) {
-                    delay(10);
+                while (millis() - start < STABILIZATION_TIME) {           
+                    delay(SLEEPING_TIME);
+                    get_measure_error_and_show();
+                    print_measure_and_setpoint();
+                    check_for_command();         
 
-                    pH = get_pH();
-                    desired_pH = get_desired_pH();
-                    show_in_current_display(pH);
-                    show_in_desired_display(desired_pH);
-                    Serial.print("pH: ");
-                    Serial.print(pH);
-                    Serial.print("\t\tdesired_pH: ");
-                    Serial.println(desired_pH);
-
-                    error = pH - desired_pH;
-                    check_for_command();
+                  
                 }
             } while (abs(error) > STABILIZATION_MARGIN);
         }
@@ -158,10 +168,26 @@ void loop() {
     delay(SLEEPING_TIME);
 }
 
+void get_measure_error_and_show(){
+    pH = get_pH();
+    desired_pH = get_desired_pH();
+    show_in_current_display(pH);
+    show_in_desired_display(desired_pH);     
+    error = pH - desired_pH;
+}
+
+void print_measure_and_setpoint(){
+    Serial.print("pH: ");
+    Serial.print(pH);
+    Serial.print("\t\tdesired_pH: ");
+    Serial.println(desired_pH);
+}
+
 void check_for_command() {
     if (Serial.available() > 0) {
         String msg = Serial.readString();
         JSONVar myObject = JSON.parse(msg);
+        JSONVar Data;
         String command = "NONE";
         command = myObject["COMMAND"];
         if (command.equals("PHREAD")) {
@@ -187,7 +213,20 @@ void check_for_command() {
             manualMode = true;
             Data["MSG"] = msg;
             Data["ACK"] = "DONE";
-        } else {
+        } else if (command.equals("SET_PH")) {            
+            desired_pH_cmd = command.equals("VALUE");
+            desired_pH_to_eeprom(desired_pH_cmd);
+            desired_pH_from_cmd = true;
+            Data["MSG"] = msg;
+            Data["ACK"] = "DONE";
+        } else if (command.equals("DESIRED_SOURCE")) {                    
+            
+            desired_pH_from_cmd = command.equals("VALUE").equals("CMD");
+            Data["FROM_CMD"] = desired_pH_from_cmd;
+            Data["MSG"] = msg;
+            Data["ACK"] = "DONE";
+        }
+        else {
             Data["ACK"] = "ERROR";
             Data["MSG"] = msg;
         }
@@ -196,9 +235,16 @@ void check_for_command() {
     }
 }
 
+//----------------------//
+
+//---- SENSORS SECTION  ---------//
 float get_desired_pH() {
-    return map(analogRead(POT_PIN), 0, 1023, 50, 70) / 10.0;
+    if(desired_pH_from_cmd)
+        return desired_pH_cmd;
+    else
+        return map(analogRead(POT_PIN), 0, 1023, 50, 70) / 10.0;
 }
+
 
 float get_pH() {
     if (pH_Serial.available() > 0) {
@@ -234,7 +280,9 @@ void pH_down(int dropTime) {
     delay(dropTime);
     analogWrite(M_PH_DN, ZERO_SPEED);
 }
+//----------------------//
 
+//---- DISPLAYS  ---------//
 void show_in_desired_display(float ph) {
     int phWithoutDecimals = ph * 10;
 
