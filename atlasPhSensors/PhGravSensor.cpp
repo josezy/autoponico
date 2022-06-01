02afbd473c7dc9aa6b097ec59e1ca25f092efc56
abcei2 <santiago.salgado.duque@gmail.com>
2022-06-01 17:57:14 -0500

    .

--- atlasPhSensors/PhGravSensor.cpp
+++ atlasPhSensors/PhGravSensor.cpp
@@ -1,11 +1,14 @@
 #include "PhGravSensor.h"
 
-#include "ph_grav.h"     
-PhGravSensor::PhGravSensor(int phAnalogPin)
+#include "ph_grav.h"
+PhGravSensor::PhGravSensor(const int phAnalogPin)
 {
-    this->pH = new Gravity_pH(A0);
+    this->pH = new Gravity_pH(phAnalogPin);
 }
-
+bool PhGravSensor::init()
+{
+    return this->pH->begin();
+}
 float PhGravSensor::getPh()
 {
     return this->pH->read_ph();
--- atlasPhSensors/PhGravSensor.h
+++ atlasPhSensors/PhGravSensor.h
@@ -1,15 +1,15 @@
 #ifndef PH_GRAV_SENSOR_H
 #define PH_GRAV_SENSOR_H
 
+#include "ph_grav.h"
 
-#include "ph_grav.h"       
+class PhGravSensor
+{
+    Gravity_pH *pH;
 
-class PhGravSensor {    
-    Gravity_pH* pH;   
-    public:  
-        PhGravSensor(int phAnalogPin);
-        bool init();
-        float getPh();	
-
+public:
+    PhGravSensor(int phAnalogPin);
+    float getPh();
+    bool init();
 };
 #endif
