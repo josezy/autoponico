8affd2e062ce280ff7ee66245f00a2fd84d8562e
Jose B <jose.zdy@gmail.com>
2022-08-28 21:49:15 -0500

    try fixing typo

--- utils/ec_serial/ec_serial.ino
+++ utils/ec_serial/ec_serial.ino
@@ -56,7 +56,7 @@ void loop() {                                         //here we go...
 
 
   if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
-    if (isdigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
+    if (isDigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
       Serial.println(sensorstring);                   //send that string to the PC's serial monitor
     }
     else                                              //if the first character in the string is NOT a digit
