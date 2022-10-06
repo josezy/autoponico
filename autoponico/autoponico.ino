64f5e5173a70f549ffbb7c89ceb5a03dac202f00
Jose <jose.zdy@gmail.com>
2022-10-06 17:37:22 -0500

    Update autoponico.ino

--- autoponico/autoponico.ino
+++ autoponico/autoponico.ino
@@ -69,7 +69,7 @@ void setup() {
     phControl.setReadSetPointFromCMD(sensorEEPROM.readFromCmd());
     lastMillis = millis();
     pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
+    digitalWrite(13, HIGH);
 }
 
 void loop() {
