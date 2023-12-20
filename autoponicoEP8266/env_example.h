3f46789c44a4f366313f2d0c97291ae7b924a402
abcei2 <santiago.salgado.duque@gmail.com>
2023-12-20 18:43:49 -0500

    env_example file

---
+++ autoponicoEP8266/env_example.h
@@ -0,0 +1,10 @@
+#define INFLUXDB_URL ""
+#define INFLUXDB_TOKEN ""
+#define INFLUXDB_ORG ""
+#define INFLUXDB_BUCKET ""
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
+// PiSocket
+#define PISOCKET_URL ""
+// Wifi
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
