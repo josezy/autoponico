cd6a57a62ff5b926a1495e471379bd620c187ce3
Jose B <jose.zdy@gmail.com>
2023-12-26 18:59:04 -0500

    yarn.lock + nodemon + typo

--- ws-server/main.ts
+++ ws-server/main.ts
@@ -19,7 +19,7 @@ wss.on("connection", (ws: WebSocketServer) => {
  
     // handling what to do when clients disconnects from server
     ws.on("close", () => {
-        console.log("the client has connected");
+        console.log("the client has disconnected");
     });
     // handling client connection error
     ws.onerror = function () {
