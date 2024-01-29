32cfe4228b3423502b439d777223a54986d8f205
Jose B <jose.zdy@gmail.com>
2024-01-29 14:08:04 -0500

    format

--- arduino/libraries/SimpleKalmanFilter/src/SimpleKalmanFilter.h
+++ arduino/libraries/SimpleKalmanFilter/src/SimpleKalmanFilter.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * SimpleKalmanFilter - a Kalman Filter implementation for single variable models.
  * Created by Denys Sene, January, 1, 2017.
  * Released under MIT License - see LICENSE file for details.
@@ -7,25 +7,22 @@
 #ifndef SimpleKalmanFilter_h
 #define SimpleKalmanFilter_h
 
-class SimpleKalmanFilter 
-{
+class SimpleKalmanFilter {
+   public:
+    SimpleKalmanFilter(float mea_e, float est_e, float q);
+    float updateEstimate(float mea);
+    void setMeasurementError(float mea_e);
+    void setEstimateError(float est_e);
+    void setProcessNoise(float q);
+    float getKalmanGain();
 
-public:
-  SimpleKalmanFilter(float mea_e, float est_e, float q);
-  float updateEstimate(float mea);
-  void setMeasurementError(float mea_e);
-  void setEstimateError(float est_e);
-  void setProcessNoise(float q);
-  float getKalmanGain();
-  
-private:
-  float _err_measure;
-  float _err_estimate;
-  float _q;
-  float _current_estimate;
-  float _last_estimate;
-  float _kalman_gain;
-  
+   private:
+    float _err_measure;
+    float _err_estimate;
+    float _q;
+    float _current_estimate;
+    float _last_estimate;
+    float _kalman_gain;
 };
 
 #endif
