0506286a860119eba349fc9225fd17f955992b94
Jose B <jose.zdy@gmail.com>
2025-07-14 13:11:57 -0500

    env var

--- webapp/src/app/api/notifications/route.ts
+++ webapp/src/app/api/notifications/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend('re_DTuSSejs_NkNmEdMCJjtHgaQUTgphxDHJ');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   try {
